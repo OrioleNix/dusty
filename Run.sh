0761e586cac9ee8420ddd0bd9c1373b140eb16e4
OrioleNix <163466443+OrioleNix@users.noreply.github.com>
2025-08-09 20:33:16 -0400

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y labwc swaybg xfce4-appfinder thunar foot firefox-esr network-manager blueman vlc orchis-gtk-theme faba-icon-theme mako-notifier swayidle swaylock greetd gtkgreet wlogout cage desktop-base sway fish gnome-software lxpolkit
-sudo useradd -M -G video greeter
-sudo chmod -R go+r /etc/greetd
-cd configs
-sudo cp greetd.toml /etc/greetd/config.toml
-sudo cp greetdenv /etc/greetd/environments
-sudo cp sway /etc/greetd/sway-config
-sudo cp -r OB-Orchis-OD/ /usr/share/themes/OB-Orchis-OD
-mkdir ~/.config/
-mkdir ~/.config/labwc/
-cp labmenu ~/.config/labwc/menu.xml
-cp autostart ~/.config/labwc/autostart
-systemctl reboot
+sudo apt install git
+git clone https://github.com/OrioleNix/dusty.git
+cd dusty
+bash config.sh
